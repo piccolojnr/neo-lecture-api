641a13bb0a0a457a58462647738ca41475039c1d
Rahim Daud <rahimdaud246@gmail.com>
2024-12-07 22:07:37 +0000

    Increase request body size limit for JSON and URL-encoded data

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,8 @@ app.options('*', (req, res) => {
   res.sendStatus(200);
 });
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
 
 
