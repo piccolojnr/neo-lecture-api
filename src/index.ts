0ffaf0c5f8a47745ac73a3fca2204687dc43e65c
Rahim Daud <rahimdaud246@gmail.com>
2024-12-07 15:05:42 +0000

    Refactor middleware configuration to use default settings for helmet and CORS

--- src/index.ts
+++ src/index.ts
@@ -29,26 +29,11 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middleware
-app.use(helmet({
-  crossOriginEmbedderPolicy: false,
-  contentSecurityPolicy: false,
-}));
-app.use(cors({
-  origin: '*', // Allow all origins
-  credentials: true,
-}));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.options('*', cors()); // Preflight requests for all routes
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 
 // Serve uploaded files
